616419822bb0f7bfdce0a9ec03f068301609d99f
Jintao Huang <hjt_study@qq.com>
2020-06-08 19:58:28 +0800

    small changes

--- pred_image.py
+++ pred_image.py
@@ -3,7 +3,7 @@
 
 from models.efficientdet import efficientdet_d0
 import torch
-from utils.predictor import Predictor
+from utils.detect.predictor import Predictor
 
 # -------------------------------- 参数
 image_path = "images/1.png"
@@ -19,4 +19,4 @@ else:
 model = efficientdet_d0(True)
 predictor = Predictor(model, device)
 predictor.pred_image_and_save(image_path, None, "max", score_thresh, nms_thresh)
-predictor.pred_image_and_show(image_path, "max", score_thresh, nms_thresh)  # test function
+# predictor.pred_image_and_show(image_path, "max", score_thresh, nms_thresh)  # test function
