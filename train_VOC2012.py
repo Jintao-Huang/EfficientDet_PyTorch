b8d6b0113847ec67b6767b36dbdb425b736e4436
Jintao Huang <hjt_study@qq.com>
2021-03-30 23:10:12 +0800

    update

--- train_VOC2012.py
+++ train_VOC2012.py
@@ -19,18 +19,20 @@ labels_map = VOC_Dataset.labels_int2str
 def lr_func(epoch):
     if 0 <= epoch < 1:
         return 1e-4
-    elif 1 <= epoch < 2:
+    elif 1 <= epoch < 3:
         return 1e-3
-    elif 2 <= epoch < 4:
+    elif 3 <= epoch < 5:
         return 0.01
-    elif 4 <= epoch < 6:
+    elif 5 <= epoch < 10:
         return 0.025
-    elif 6 <= epoch < 100:
+    elif 10 <= epoch < 80:
         return 0.05
-    elif 100 <= epoch < 116:
+    elif 80 <= epoch < 110:
         return 0.02
-    elif 116 <= epoch < 120:
+    elif 110 <= epoch < 115:
         return 5e-3
+    elif 115 <= epoch < 120:
+        return 1e-4
 
 
 def main():
@@ -43,12 +45,12 @@ def main():
 
     optim = torch.optim.SGD(model.parameters(), 0, 0.9, weight_decay=4e-5)
     train_dataset = VOC_Dataset(VOCDetection(voc_dir, image_set="train"))
-    test_dataset = VOC_Dataset(VOCDetection(voc_dir, image_set="val"))
+    val_dataset = VOC_Dataset(VOCDetection(voc_dir, image_set="val"))
     ap_counter = APCounter(labels_map, 0.5)
     writer = SummaryWriter(comment=comment)
     logger = Logger(50, writer)
     checker = Checker(Tester(model, train_dataset, batch_size, device, ap_counter, 1000),
-                      Tester(model, test_dataset, batch_size, device, ap_counter, 1000),
+                      Tester(model, val_dataset, batch_size, device, ap_counter, 1000),
                       Saver(model), logger, 8, 2)
     lr_scheduler = LRScheduler(optim, lr_func)
     trainer = Trainer(model, optim, train_dataset, batch_size, device, lr_scheduler, logger, checker)
